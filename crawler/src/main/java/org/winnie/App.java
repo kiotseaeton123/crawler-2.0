514f8499be7d5e20d646c5277c8ea74aee4e1025
winnie houng <kiots@LAPTOP-4E9ES74J>
2024-09-19 05:26:04 -0500

    update gitignore

--- crawler/src/main/java/org/winnie/App.java
+++ crawler/src/main/java/org/winnie/App.java
@@ -2,7 +2,6 @@ package org.winnie;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.*;
 import java.util.Random;
