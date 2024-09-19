f622935a6bf5fde979b86e32a7c3130ee925fbf3
winnie houng <kiots@LAPTOP-4E9ES74J>
2024-09-18 21:23:41 -0500

    automated crawling, crude handling of empty links

--- crawler/src/main/java/org/winnie/App.java
+++ crawler/src/main/java/org/winnie/App.java
@@ -4,51 +4,48 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.net.*;
 import java.io.*;
+import java.util.Random;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        scrapeTopic("/wiki/Java");
+public class App {
+    public static void main(String[] args) {
+        scrapetopic("/wiki/Java");
     }
 
-    public static void scrapeTopic(String path){
-        String html=getUrl("https://www.wikipedia.org/"+path);
-        Document document=Jsoup.parse(html);
-       
-        // select paragraphs <p>
-        Elements paragraphs=document.select("p");
-        for(Element p:paragraphs){
-            System.out.println(p.text());
-        }
-    }
+    public static void scrapetopic(String path) {
+        String url="https://www.wikipedia.org"+path;
 
-    public static String getUrl(String url){
-        URL urlobject=null;
-        try{
-            urlobject=new URL(url);
-        }catch(MalformedURLException e){
-            System.out.println("url malformed");
-            return "";
-        }
+        try {
+            // get html and select css tags
+            Document document = Jsoup.connect(url).get();
+            Elements paragraphs = document.select("p");
+            Elements links=document.select("a[href]");
 
-        URLConnection connection=null;
-        BufferedReader in=null;
-        String output="";
-        try{
-            connection=urlobject.openConnection();
-            in=new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line="";
-            while((line=in.readLine())!=null){
-                output+=line;
+            // number elements in html
+            int nparagraphs=paragraphs.size();
+            int nlinks=links.size();
+
+            // crawl links if there are links
+            if(nlinks>0 && nparagraphs>0){
+                System.out.println("here");
+                Random random=new Random();
+                String randomlink=links.get(random.nextInt(nlinks)).attr("href");
+                String firstparagraph=paragraphs.get(0).text();
+
+                if(!randomlink.isEmpty() && randomlink.startsWith("/wiki")){
+                    System.out.println("link: "+randomlink);
+                    System.out.println("first paragraph: "+firstparagraph);
+                    scrapetopic(randomlink);
+                }
+                else{
+                    System.out.println("------------------------");
+                    scrapetopic("/wiki/Java");
+                }
             }
-            in.close();
-        }catch(IOException e){
-            System.out.println("error connecting to url");
-            return "";
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return output;
+
     }
 }
